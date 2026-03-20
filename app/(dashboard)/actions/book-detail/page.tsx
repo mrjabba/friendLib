5f40a545e866f8687da131da671122d0955c9066
Kevin Hutson <mrjabba@gmail.com>
2026-03-19 19:19:10 -0500

    forgot to add these

---
+++ app/(dashboard)/actions/book-detail/page.tsx
@@ -0,0 +1,13 @@
+//import {Book} from './models';
+
+import { Book } from "@/app/models";
+
+export default function BookDetail({book}: {book: Book}) {
+    return (
+        <>
+        <h3>{book.title}</h3>
+        <h4>{book.author}</h4>
+        <h4>{book.pages}</h4>
+        </>
+    )
+}
